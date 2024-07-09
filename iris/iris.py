a724daecb8523dbf89f78f390bec62af71d637ea
erenyurtcu <ierenyurtcu@gmail.com>
2024-07-09 15:21:52 +0300

    last

--- iris/iris.py
+++ iris/iris.py
@@ -1,5 +1,98 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import confusion_matrix
 
-veriler = pd.read_excel('iris.xls')
+data = pd.read_excel('iris.xls')
+
+x = data.iloc[:,1:4].values
+y = data.iloc[:,4:].values
+print(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.33, random_state=0)
+
+# scaling data
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+# logistic regression
+from sklearn.linear_model import LogisticRegression
+logReg = LogisticRegression(random_state=0)
+logReg.fit(X_train,y_train)
+y_pred = logReg.predict(X_test)
+print(y_pred)
+print(y_test)
+
+# confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+
+# KNN
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=1, metric='minkowski')
+knn.fit(X_train,y_train)
+y_pred = knn.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('KNN')
+print(cm)
+
+# SVC - Support Vector Machine
+from sklearn.svm import SVC
+svc = SVC(kernel='rbf')
+svc.fit(X_train,y_train)
+y_pred = svc.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('SVC')
+print(cm)
+
+# naive bayes
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+y_pred = gnb.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('GNB')
+print(cm)
+
+# decision tree
+from sklearn.tree import DecisionTreeClassifier
+dtc = DecisionTreeClassifier(criterion = 'entropy')
+
+dtc.fit(X_train,y_train)
+y_pred = dtc.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('DTC')
+print(cm)
+
+# random forest
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators=10, criterion = 'entropy')
+rfc.fit(X_train,y_train)
+y_pred = rfc.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('RFC')
+print(cm)
+
+# ROC / TPR / FPR values
+
+y_proba = rfc.predict_proba(X_test)
+print(y_test)
+print(y_proba[:,0])
+
+from sklearn import metrics
+fpr , tpr , thold = metrics.roc_curve(y_test,y_proba[:,0],pos_label='e')
+print(fpr)
+print(tpr)
+
+
