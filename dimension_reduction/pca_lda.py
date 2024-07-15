d9c333c6534d6dc0a30b3ffb02315d718460679e
erenyurtcu <ierenyurtcu@gmail.com>
2024-07-15 23:35:52 +0300

    lda
    lda dimension reduction added
    lda vs pca efficiency compared

--- dimension_reduction/pca_lda.py
+++ dimension_reduction/pca_lda.py
@@ -25,11 +25,11 @@ X_test2 = pca.transform(X_test)
 
 # Logistic Regression before PCA transformation
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state=0)
+classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Logistic Regression after PCA transformation
-classifier2 = LogisticRegression(random_state=0)
+classifier2 = LogisticRegression(random_state = 0)
 classifier2.fit(X_train2, y_train)
 
 # predictions
@@ -52,3 +52,24 @@ print(cm2)
 print('without PCA and with PCA')
 cm3 = confusion_matrix(y_pred, y_pred2)
 print(cm3)
+
+# LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+lda = LDA(n_components = 2)
+
+X_train_lda = lda.fit_transform(X_train, y_train)
+X_test_lda = lda.transform(X_test)
+
+# After LDA transformation
+classifier_lda = LogisticRegression(random_state=0)
+classifier_lda.fit(X_train_lda, y_train)
+
+# Predict using LDA data
+y_pred_lda = classifier_lda.predict(X_test_lda)
+
+# LDA results / original results
+print('LDA and original')
+cm4 = confusion_matrix(y_pred, y_pred_lda)
+print(cm4)
+
